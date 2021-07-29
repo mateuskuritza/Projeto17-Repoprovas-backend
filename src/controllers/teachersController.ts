3616dbf2bb93b7c317850858f9be0cb36aca4ab6
Mateus_Kuritza <mateuskuritza@gmail.com>
2021-07-28 22:10:07 -0300

    teachers first routes

--- src/controllers/teachersController.ts
+++ src/controllers/teachersController.ts
@@ -3,6 +3,36 @@ import { getRepository } from "typeorm";
 import Teachers from "../entities/Teachers";
 
 export async function getAll(req: Request, res: Response) {
-    const allTeachers = await getRepository(Teachers).find();
-    res.status(200).send(allTeachers);
+    try {
+        const allTeachers = await getRepository(Teachers).find();
+        res.status(200).send(allTeachers);
+    } catch (e) {
+        console.log(e);
+        res.sendStatus(500);
+    }
+}
+
+export async function getTeacherById(req: Request, res: Response) {
+    try {
+        const teacher = await getRepository(Teachers).findOne({
+            where: {
+                id: req.params.id
+            }
+        });
+        if (!teacher) return res.sendStatus(404);
+        res.status(200).send(teacher);
+    } catch (e) {
+        console.log(e);
+        res.sendStatus(500);
+    }
+}
+
+export async function createTeacher(req: Request, res: Response) {
+    try {
+        const teacher = await getRepository(Teachers).save(req.body);
+        res.status(201).send(teacher);
+    } catch (e) {
+        console.log(e);
+        res.sendStatus(500);
+    }
 }
--- src/routes/teachers.ts
+++ src/routes/teachers.ts
@@ -2,6 +2,9 @@ import express from "express";
 const router = express.Router();
 import * as teachersController from "../controllers/teachersController";
 
-router.use("/", teachersController.getAll);
+router.get("/:id", teachersController.getTeacherById);
+router.get("/", teachersController.getAll);
+
+router.post("/", teachersController.createTeacher);
 
 export default router;
