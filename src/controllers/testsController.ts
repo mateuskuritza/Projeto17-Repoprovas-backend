2685acaaf895758c51204b3fa4c05b831281a514
Mateus_Kuritza <mateuskuritza@gmail.com>
2021-07-28 22:08:55 -0300

    tests first routes

---
+++ src/controllers/testsController.ts
@@ -0,0 +1,66 @@
+import { Request, Response } from "express";
+import { getRepository } from "typeorm";
+import Tests from "../entities/Tests";
+
+export async function createTest(req: Request, res: Response) {
+    try {
+        const test = await getRepository(Tests).save(req.body);
+        res.status(201).send(test);
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
+}
+
+export async function getAll(req: Request, res: Response) {
+    try {
+        const allTests = await getRepository(Tests).find();
+        res.status(200).send(allTests);
+    } catch (e) {
+        console.log(e);
+        res.sendStatus(500);
+    }
+}
+
+export async function getTestById(req: Request, res: Response) {
+    try {
+        const test = await getRepository(Tests).findOne({
+            where: {
+                id: req.params.id
+            }
+        });
+        if (!test) return res.sendStatus(404);
+        res.status(200).send(test);
+    } catch (e) {
+        console.log(e);
+        res.sendStatus(500);
+    }
+}
+
+export async function getBySubjectId(req: Request, res: Response) {
+    try {
+        const tests = await getRepository(Tests).find({
+            where: {
+                subjectId: req.params.subjectId
+            }
+        });
+        res.status(200).send(tests);
+    } catch (e) {
+        console.log(e);
+        res.sendStatus(500);
+    }
+}
+
+export async function getByTeacherId(req: Request, res: Response) {
+    try {
+        const tests = await getRepository(Tests).find({
+            where: {
+                teacherId: req.params.teacherId
+            }
+        });
+        res.status(200).send(tests);
+    } catch (e) {
+        console.log(e);
+        res.sendStatus(500);
+    }
+}
---
+++ src/routes/tests.ts
@@ -0,0 +1,13 @@
+import express from "express";
+const router = express.Router();
+import * as testsController from "../controllers/testsController";
+
+router.post("/", testsController.createTest);
+
+router.get("/teacher/:id", testsController.getByTeacherId);
+router.get("/subject/:id", testsController.getBySubjectId);
+router.get("/:id", testsController.getTestById);
+router.get("/", testsController.getAll);
+
+
+export default router;
