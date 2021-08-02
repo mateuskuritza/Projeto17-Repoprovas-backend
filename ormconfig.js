b714abbe3504ae3f1891b4ece25798deaed0f6e6
Mateus_Kuritza <mateuskuritza@gmail.com>
2021-08-02 11:49:41 -0300

    typeorm heroku

--- ormconfig.js
+++ ormconfig.js
@@ -4,13 +4,13 @@ module.exports = {
     type: "postgres",
     url: process.env.DATABASE_URL,
     migrationsTableName: "migrations",
-    entities: ["dist/entities/*.js"],
-    migrations: ["dist/migrations/*.js"],
+    entities: ["dist/src/entities/*.js"],
+    migrations: ["dist/src/migrations/*.js"],
     ssl: {
         rejectUnauthorized: false,
     },
     cli: {
         migrationsDir: "src/migrations",
-        entitiesDir: "dist/entities/*.js"
+        entitiesDir: "dist/src/entities/*.js"
     }
 };
